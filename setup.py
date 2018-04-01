15c9da64667b164083937082307fa7039d216616
lucker7777 <holanga4321@gmail.com>
2018-04-01 20:14:21 +0200

    a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='parallel_ga_processing',
-    version='1.0.3',
+    version='1.0.4',
     packages=['examples', 'algorithmRunners', 'geneticAlgorithms'],
     url='https://github.com/lucker7777/parallelGA',
     license='',
